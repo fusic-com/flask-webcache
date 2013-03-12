c16bce0c6dda8901b53499417549591d621c130c
Yaniv Aknin <yaniv@aknin.name>
2013-03-12 11:34:15 +0200

    Consider validation of cached responses as well
    So cached responses can also benefit from the bandwidth reduction of a
    304 response.

--- flask_webcache/handlers.py
+++ flask_webcache/handlers.py
@@ -26,8 +26,6 @@ class ResponseHandler(validation.Validation, storage.Store):
     def init_app(self, app):
         app.after_request(self.after_request)
     def after_request(self, response):
-        if g.webcache_cached_response:
-            return response
         self.add_date_fields(response)
         for modifier in modifiers.after_request:
             modifier(response)
@@ -35,6 +33,8 @@ class ResponseHandler(validation.Validation, storage.Store):
             self.set_etag(response)
         if self.if_none_match(response):
             return self.return_not_modified_response(response) or response
+        if g.webcache_cached_response:
+            return response
         if self.should_cache_response(response) and not self.is_exempt():
             self.cache_response(response)
             self.mark_cache_miss(response)
---
+++ tests/test_handler.py
@@ -0,0 +1,29 @@
+import unittest
+from httplib import NOT_MODIFIED
+
+from flask import Flask
+from flask_webcache import easy_setup
+
+class HandlerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.a = Flask(__name__)
+        easy_setup(self.a)
+        @self.a.route('/foo')
+        def foo():
+            return 'bar'
+
+    def test_full_cycle(self):
+        first = self.a.test_client().get('/foo')
+        second = self.a.test_client().get('/foo')
+        self.assertIn('x-cache', first.headers)
+        self.assertIn('x-cache', second.headers)
+        self.assertEquals(first.headers['x-cache'], 'miss')
+        self.assertEquals(second.headers['x-cache'], 'hit')
+        self.assertEquals(first.data, second.data)
+
+    def test_not_modified_cached_response(self):
+        first = self.a.test_client().get('/foo')
+        self.assertIn('etag', first.headers)
+        second = self.a.test_client().get('/foo', headers=(("if-none-match", first.headers['etag']),))
+        self.assertEquals(second.status_code, NOT_MODIFIED)
