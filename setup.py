24fa70c4694b9840bb8a47a4623890a32ccbb449
Yaniv Aknin <yaniv@aknin.name>
2013-04-27 13:54:10 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-WebCache',
-    version='0.8',
+    version='0.8.1',
     url='http://github.com/fusic-com/flask-webcache/',
     license='BSD',
     author='Yaniv Aknin',
